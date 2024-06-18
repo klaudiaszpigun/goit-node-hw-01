0712c46eab8034682b32136c10ed1b44c11699fd
klaudiaszpigun <141838129+klaudiaszpigun@users.noreply.github.com>
2024-06-18 22:23:19 +0200

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -3,7 +3,6 @@ import { nanoid } from "nanoid";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Ustalanie __dirname dla modułów ES
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
